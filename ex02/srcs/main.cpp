214813b334b7c1a924306487b64af6430adacadb
jcameira <joao_carlos1995@hotmail.com>
2024-11-11 01:20:17 +0000

    ex02 done

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcameira <jcameira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 20:27:04 by jcameira          #+#    #+#             */
+/*   Updated: 2024/11/11 01:15:14 by jcameira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstring>
+#include <iostream>
+
+int main(){
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    std::cout << "Memory address of str: " << &str << std::endl;
+	std::cout << "Memory address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address of stringREF: " << &stringREF << std::endl;
+	std::cout << "Value of the str: " << str << std::endl;
+	std::cout << "Value of the stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of the stringREF: " << stringREF << std::endl;
+}
